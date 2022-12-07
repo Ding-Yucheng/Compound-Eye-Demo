04d5c85ed0b233818d1324a5906a1d5eedd25e24
Ding-Yucheng <111465716+Ding-Yucheng@users.noreply.github.com>
2022-12-07 22:23:36 +0800

    Update Instant_mapping_server.py

--- Instant_mapping_server.py
+++ Instant_mapping_server.py
@@ -12,12 +12,19 @@ esp.osdebug(None)
 import gc
 gc.collect()
 
+# Sampling setting
+sample_per_pixel = 50
+reject_first_samples = 10
+sampling_interval = 10 # in microseconds
+interval_between_pixel = 10 # in milliseconds
+
+# Network setting
 ssid = 'MyESP32'
 password = '12345678'
 host_ip = '192.168.137.16'
 host_port = 80
-station = network.WLAN(network.STA_IF)
 
+station = network.WLAN(network.STA_IF)
 station.active(True)
 
 while station.isconnected() == False:
@@ -41,52 +48,51 @@ A3 = Pin(32, Pin.OUT)
 M1 = Pin(27, Pin.OUT)
 M2 = Pin(14, Pin.OUT)
 M3 = Pin(13, Pin.OUT)
-PD_Cur = ADC(Pin(36, Pin.IN), atten=ADC.ATTN_11DB)
-pd_data = []
+PD_Cur = ADC(Pin(36, Pin.IN), atten = ADC.ATTN_11DB)
+
 sarr = ''
+pd_data = []
 for i in range(0, 37):
     pd_data.append(0)
 
+def select_mux(m1, m2, m3):
+    M1.value(m1)
+    M2.value(m2)
+    M3.value(m3)
+
+def select_channel(a0, a1, a2, a3):
+    A0.value(a0)
+    A1.value(a1)
+    A2.value(a2)
+    A3.value(a3)
 
 def scan():
     for i in range(4, 41):
         mux = i / 16
         pixel = i % 16
-        M1.value(0)
-        M2.value(0)
-        M3.value(0)
         if mux == 0:
-            M1.value(1)
+            select_mux(1, 0, 0)
         elif mux == 1:
-            M2.value(1)
+            select_mux(0, 1, 0)
         elif mux == 2:
-            M3.value(1)
-        A0.value(1 & (pixel >> 0))
-        A1.value(1 & (pixel >> 1))
-        A2.value(1 & (pixel >> 2))
-        A3.value(1 & (pixel >> 3))
-        time.sleep_us(10)
+            select_mux(0, 0, 1)
+        select_channel(1 & (pixel >> 0), 1 & (pixel >> 1), 1 & (pixel >> 2), 1 & (pixel >> 3))
         cache = 0
-        for j in range (0, 50):
-            cache += PD_Cur.read_uv()
-            time.sleep_us(10)
-            
-        pd_data[i - 4] = int(cache / 50)
-        ## connect to ground
-        M1.value(0)
-        M2.value(0)
-        M3.value(1)
-        A0.value(1)
-        A1.value(1)
-        A2.value(1)
-        A3.value(1)
-        time.sleep_ms(10)
+        for j in range (0, sample_per_pixel + reject_first_samples):
+            if j >= reject_first_samples:
+                cache += PD_Cur.read_uv()
+            time.sleep_us(sampling_interval)
+        pd_data[i - 4] = int(cache / sample_per_pixel)
+        ## set to floating
+        select_mux(0, 0, 1)
+        select_channel(1, 0, 1, 0)
+        time.sleep_ms(interval_between_pixel)
         
 
 def list_to_str():
     global sarr
     sarr = 'str'
-    for i in range(0,37):
+    for i in range(0, 37):
         sarr += str(pd_data[i])
         sarr += '.'
     sarr += 'end'
